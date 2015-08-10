ac6c56fa726eeb7ab59ba8b74dd9067431ae4156
ashleydonohoe <ashleyrdonohoe@gmail.com>
2015-08-09 22:22:50 -0400

    first commit

---
+++ 1 Hour Swift Tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  1 Hour Swift Tutorial
+//
+//  Created by Gabriele on 8/9/15.
+//  Copyright © 2015 Ashley Donohoe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
